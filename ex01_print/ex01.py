8caca5d57b7ea00db2037259774c523fc3011c31
InTaek Gong <git1219@naver.com>
2020-03-29 09:36:38 +0000

    ex01 study drills

--- ex01_print/ex01.py
+++ ex01_print/ex01.py
@@ -5,10 +5,13 @@
 # 각자 Study drills 시도 후 필요시 commit / Try Study Drills and commit if necessary
 # print 'abc' -> print('abc')
 
-print("Hello World!")
+# print("Hello World!")
 print("Hello Again")
 print("I like typing this.")
 print("This is fun.")
 print('yay! Printing.')
 print("I'd much rather you 'not'.")
 print('I "said" do not touch this.')
+
+print("another line (or text you want)")
+print('한 행 더 출력 (또는 희망하는 글')
