8ce760173c3c1866acf0fddb4a1ae1f950c4aa4f
InTaek Gong <git1219@naver.com>
2020-03-30 14:58:36 +0000

    ex02

--- ex02_comment/ex02.py
+++ ex02_comment/ex02.py
@@ -4,3 +4,13 @@
 # 각 행 주석 입력 후 commit / Enter comment for each line and commit
 # 각자 Study drills 시도 후 필요시 commit / Try Study Drills and commit if necessary
 # print 'abc' -> print('abc')
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this.") # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
