0dd4faa896b61ff6acd6a59a9e7d7bdda700ba3e
InTaek Gong <git1219@naver.com>
2020-05-06 11:39:31 +0000

    ex21 수정

--- ex21_return/ex21.py
+++ ex21_return/ex21.py
@@ -9,3 +9,40 @@
 # print 'abc', -> print('abc', end=" ")
 # raw_input('abc') -> input('abc')
 # 오류노트 에 각자 오류노트 작성 / Please use your error-note
+
+def 더하기(a, b):
+  print(f"덧셈 {a} + {b}")
+  return a + b
+
+
+def 빼기(a, b):
+  print(f"뺄셈 {a} - {b}")
+  return a - b
+
+
+def 곱하기(a, b):
+  print(f"곱셈 {a} * {b}")
+  return a * b
+
+
+def 나누기(a, b):
+  print(f"나눗셈 {a} / {b}")
+  return a / b
+
+
+print("함수만으로 계산해봅시다")
+
+나이_yr = 더하기(15, 10)
+키_cm = 빼기(200, 18)
+몸무게_kg = 곱하기(29, 3)
+아이큐 = 나누기(450, 3)
+
+print("나이: %d, 키: %d, 몸무게: %d, 아이큐: %d" % (나이_yr, 키_cm, 몸무게_kg, 아이큐))
+
+# 추가 점수 문제, 아무렇게나 쓰세요.
+print('\n')
+print("문제를 하나 드리겠습니다.")
+
+값 = 더하기(나이_yr, 빼기(키_cm, 곱하기(몸무게_kg, 나누기(아이큐, 2))))
+
+print("결과:", 값, "손으로 계산할 수 있나요?")
