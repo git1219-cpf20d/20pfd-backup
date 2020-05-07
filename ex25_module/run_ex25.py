f6d62d201a67e0f072f96021a52dc572d9ac1500
InTaek Gong <git1219@naver.com>
2020-05-07 21:03:11 +0000

    ex25

--- ex25_module/run_ex25.py
+++ ex25_module/run_ex25.py
@@ -9,3 +9,20 @@
 # print 'abc', -> print('abc', end=" ")
 # raw_input('abc') -> input('abc')
 # 오류노트 에 각자 오류노트 작성 / Please use your error-note
+import ex25
+
+sentence = "All good things come to those who wait."
+words = ex25.brak_words(sentence)
+print("words = %s" % words)
+sorted_words = ex25.sort_words(words)
+print("sorted_words = %s" % sorted_words)
+ex25.print_first_word(words)
+ex25.print_last_word(words)
+print("words = %s" % words)
+ex25.print_first_word(sorted_words)
+ex25.print_last_word(sorted_words)
+print("sorted_words = %s" % sorted_words)
+sorted_words = ex25.sort_sentence(sentence)
+print("sorted_words = %s" % sorted_words)
+ex25.print_first_word(sentence)
+ex25.print_last_word(sentence)
