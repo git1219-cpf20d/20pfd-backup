57d04b58383b05b6ca109e0b17d058ecb62bf26b
InTaek Gong <git1219@naver.com>
2020-05-08 05:49:17 +0000

    ex25 수정

--- ex25_module/run_ex25.py
+++ ex25_module/run_ex25.py
@@ -12,7 +12,7 @@
 import ex25
 
 sentence = "All good things come to those who wait."
-words = ex25.brak_words(sentence)
+words = ex25.break_words(sentence)
 print("words = %s" % words)
 sorted_words = ex25.sort_words(words)
 print("sorted_words = %s" % sorted_words)
