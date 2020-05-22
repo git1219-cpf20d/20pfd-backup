db5df0e6c2c41adc9a42e23ee523ceb039b08a96
InTaek Gong <git1219@naver.com>
2020-05-22 10:45:20 +0000

    ex35_5

--- ex35_5_recursion/recursion_example.py
+++ ex35_5_recursion/recursion_example.py
@@ -4,3 +4,27 @@
 # 재귀호출로 factorial 을 계산하는 프로그램을 작성하시오
 # Please write a program that calculates the factorial using the recursion
 # 오류노트 에 각자 오류노트 작성 / Please use your error-note
+
+count = 0
+memo = {}
+
+def factorial(n):
+
+  global count, memo
+  count += 1
+  print(f"factorial({n})", count)
+  if n in memo:
+    result = memo[n]
+  elif 1 >= n:
+    result = 1
+  else:
+    result = n * factorial(n - 1)
+
+  if n not in memo:
+    memo[n] = result
+
+  return result
+
+
+for i in range(10):
+  print(f"{i:2}! = {factorial(i)}")
