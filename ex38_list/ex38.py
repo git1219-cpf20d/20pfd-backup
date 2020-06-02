55dd9c7b272312d446019a37cf8f9c2114b3dcb1
InTaek Gong <git1219@naver.com>
2020-06-02 03:31:59 +0000

    ex38

--- ex38_list/ex38.py
+++ ex38_list/ex38.py
@@ -9,3 +9,25 @@
 # print 'abc', -> print('abc', end=" ")
 # raw_input('abc') -> input('abc')
 # 오류노트 에 각자 오류노트 작성 / Please use your error-note
+
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print("What there are not 10 things in that list. Let's fix that.")
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+while 10 != (len(stuff)):
+  next_one = more_stuff.pop()
+  print("Adding: %s" % next_one)
+  stuff.append(next_one)
+  print("There are %d items now." % len(stuff))
+
+print("There we go :%s" % str(stuff))
+print("Let's do some things with stuff")
+
+print("stuff[1] = %s" % stuff[1])
+print("stuff[-1] = %s" % stuff[-1])
+print("stuff.pop() = %s" % stuff.pop())
+print(' '.join(stuff))
+print('#'.join(stuff[3:5]))
